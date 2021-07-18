15992cbec5ea2df8b18c60e6ec989e7109d172b5
Vanessa Carraro <81984134+B1olog4@users.noreply.github.com>
2021-07-18 19:08:18 -0300

    Add files via upload

---
+++ projeto-desafio1/script.js
@@ -0,0 +1,22 @@
+const form =document.getElementById("formulario")
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    alert('teste ok'); //apenas para testar (não esta funcionando )
+    let nome= document.getElementById('nome').value;
+    let email= document.getElementById('email').value;
+    console.log(nome, email)
+    let data = {
+        nome,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+    localStorage.setItem('lead',convertData);
+    let content= document.getElementById('content');
+    let carregando = carregando;
+    let pronto = pronto ;
+    content.innerHTML = carregando;
+    setTimeout(()=> {
+        content.innerHTML = pronto;
+    },500)
+})
